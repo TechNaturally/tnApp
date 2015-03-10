52cd3eb9f72c1586274eebca86e7624b91201b3c
Pat Long <plong00@gmail.com>
2015-03-10 14:44:12 -0700

    added tn-tabs directive

--- www/tnApp/modules/tnUtility/tnUtility.module.js
+++ www/tnApp/modules/tnUtility/tnUtility.module.js
@@ -1,4 +1,4 @@
-angular.module('tnApp.utility', [])
+angular.module('tnApp.utility', ['tnApp.theme'])
 .filter('count', function(){
 	return function(input){
 		if(angular.isDefined(input.length)){
@@ -9,7 +9,15 @@ angular.module('tnApp.utility', [])
 		}
 		return null;
 	};
-});
+})
+.directive('tnTabs', ['Theme', function(Theme){
+	return {
+		restrict: 'E',
+		scope: {'tabs': '=actions'},
+		templateUrl: function(elem, attr){ return Theme.getTemplate(elem, attr, 'tnUtility'); },
+		controller: function(){}
+	};
+}]);
 
 Object.count = function(obj){
 	var size = 0, key;
