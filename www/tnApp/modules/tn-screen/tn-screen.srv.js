70e5abe26219d631298628fd8a561aa499734cb6
Pat Long <plong00@gmail.com>
2015-03-13 21:20:38 -0700

    TODO moving screen config to client

--- www/tnApp/modules/tn-screen/tn-screen.srv.js
+++ www/tnApp/modules/tn-screen/tn-screen.srv.js
@@ -1,5 +1,9 @@
 angular.module('tnApp.screen')
 .factory('Screen', ['$q', 'API', function($q, API){
+
+	// TOOD: implement screenProvider - inject into module configs and add screens from module's screens.json
+	// TODO: imlement Screen service which does the loading part here...
+	// TODO: - port PHP Screen into Screen service
 	
 	var api = {
 		load: function(path){
